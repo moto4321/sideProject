e18c5860ea5880c968077a9b2de87a951beaacb2
moto4321 <moto432195@gmail.com>
2021-02-07 21:15:47 +0900

    210207

--- server/server.js
+++ server/server.js
@@ -1,14 +1,4 @@
-import app from './app';
-import config from './config/index';
-
-const {PORT} = config
-
-app.listen(PORT, () => {
-  console.log(`Server started on Port ${PORT}`);
-});
-
-
-/*import app from "./app";
+import app from "./app";
 import config from "./config/index";
 
 import path from "path";
@@ -16,18 +6,17 @@ import greenlock from "greenlock-express";
 
 const { PORT } = config;
 
-greenlock
+/*greenlock
   .init({
     packageRoot: path.join(__dirname, "../"),
     configDir: path.join(__dirname, "../", "server/config/greenlock.d"),
-    maintainerEmail: "sideproject0214@gmail.com",
+    maintainerEmail: "moto4321@naver.com",
     cluster: false,
   })
   .serve(app, () => {
     console.log("greenlock work");
-  });
+  });*/
 
-//      app.listen(PORT, () => {
-//        console.log(`Server started on Port ${PORT}`);
-//     });
-*/
+     app.listen(PORT, () => {
+       console.log(`Server started on Port ${PORT}`);
+    });
