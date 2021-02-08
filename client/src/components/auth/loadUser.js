4bc23a1beb2332042f5a8b17cf5a20380ccc952c
moto4321 <moto432195@gmail.com>
2021-02-08 21:22:34 +0900

    210208

--- client/src/components/auth/loadUser.js
+++ client/src/components/auth/loadUser.js
@@ -1,19 +1,3 @@
-/*import store from '../../store';
-import { USER_LOADING_REQUEST } from '../../redux/types';
-
-const loadUser = () => {
-  try {
-    store.dispatch({
-      type: USER_LOADING_REQUEST,
-      payload: localStorage.getItem("token"),
-    })
-  } catch(e) {
-    console.log(e)
-  }
-};
-
-export default loadUser;*/
-
 import store from "../../store";
 import { USER_LOADING_REQUEST } from "../../redux/types";
 
