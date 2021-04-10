aed2e72955bd1bcadedbe79a56d54459ab56f39e
moto4321 <moto432195@gmail.com>
2021-04-10 20:52:10 +0900

    202104102

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -105,7 +105,7 @@ const AppNavbar = () => {
       <Navbar color="dark" dark expand="lg" className="sticky-top">
         <Container>
           <Link to="/" className="text-white text-decoration-none">
-            기억보단 기록을(@woooseogi)
+            Development Blog(@woooseogi)
           </Link>
           <NavbarToggler onClick={handleToggle} />
           <Collapse isOpen={isOpen} navbar>
